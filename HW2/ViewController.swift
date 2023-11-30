3aa8f1fbf1f8fad037747619ab370d8dd51a183f
Nur <mirrorvol@gmail.com>
2023-11-30 10:31:42 +0400

    UI impruvments Bag fix Code optimization

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redFlag: UIView!
+    @IBOutlet var yellowFlag: UIView!
+    @IBOutlet var greenFlag: UIView!
+    @IBOutlet var startButton: UIButton!
+    var counter = 1
+    var dimmedColor = 0.3
+    var normalColor = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 80
+        redFlag.layer.cornerRadius = 60
+        yellowFlag.layer.cornerRadius = 60
+        greenFlag.layer.cornerRadius = 60
+        
+        redFlag.alpha = dimmedColor
+        yellowFlag.alpha = dimmedColor
+        greenFlag.alpha = dimmedColor
     }
-
-
+    @IBAction func startButtonIsPressed() {
+        startButton.setTitle("Blink!", for: .normal)
+        redFlag.alpha = dimmedColor
+        yellowFlag.alpha = dimmedColor
+        greenFlag.alpha = dimmedColor
+        switch counter {
+        case 1:  redFlag.alpha = normalColor
+            counter += 1
+        case 2: yellowFlag.alpha = normalColor
+            counter += 1
+        case 3: greenFlag.alpha = normalColor
+            counter = 1
+        default:
+            print("error")
+        }
+    }
 }
-
